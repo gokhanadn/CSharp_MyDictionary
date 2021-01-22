c25b8608ea0d30dc16ab78234cda594689b85f87
Gökhan Aydın <aydingo18@itu.edu.tr>
2021-01-23 01:21:38 +0300

    Add project files.

---
+++ MyDict/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDict
+{
+    class MyDictionary<T,A>
+    {
+        T[] keys;
+        A[] values;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new A[0];
+        }
+        public void Add(T key, A value)
+        {
+            T[] tempArrayKeys = keys;
+            A[] tempArrayValues = values;
+            keys = new T[keys.Length + 1];
+            values = new A[values.Length + 1];
+            for (int i = 0; i < tempArrayKeys.Length; i++)
+            {
+                keys[i] = tempArrayKeys[i];
+                values[i] = tempArrayValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+        public int Count
+        {
+            get { return values.Length; }
+        }
+    }
+}
---
+++ MyDict/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> EmployeeList = new Dictionary<string, string>();
+            MyDictionary<string, string> EmployeeList2 = new MyDictionary<string, string>();
+            EmployeeList2.Add("Mahesh Chand", "Programmer");
+            EmployeeList2.Add("Mahesh Chand", "Programmer");
+            EmployeeList2.Add("Mahesh Chand", "Programmer");
+            EmployeeList2.Add("Mahesh Chand", "Programmer");
+            EmployeeList2.Add("Mahesh Chand", "Programmer");
+
+            Console.WriteLine(EmployeeList2.Count);
+
+        }
+    }
+}
